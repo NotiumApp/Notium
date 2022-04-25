093719f2a3979b3c03c7de6ff76ec0c65c34c79c
Shubham Patil <shubhampatilsd@gmail.com>
2022-04-24 17:42:24 -0700

    yay sockets work

--- api/src/server/Server.ts
+++ api/src/server/Server.ts
@@ -4,14 +4,28 @@ import { firebaseAdminSetup } from "../util/FirebaseAdminSetup";
 import { prisma } from "../api/v1/db/index";
 import { isAuthenticated } from "../api/v1/middlewares/isAuthenticated";
 import cors from "cors";
-
+import http from "http";
+import { Server as SocketServer } from "socket.io";
+import FirebaseAdmin from "firebase-admin";
 
 //Server configuration and setup
 export class Server {
   public app: Application;
+  //type later
+  public server;
+  public io: SocketServer;
 
   public constructor() {
     this.app = express();
+
+    this.server = http.createServer(this.app);
+
+    this.io = new SocketServer(this.server, {
+      cors: {
+        origin: "*",
+        methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
+      },
+    });
 
     //register server middlewares
     this.registerMiddlewares();
@@ -19,12 +33,15 @@ export class Server {
     //register server base routes
     this.registerRoutes();
 
+    this.registerSocketEvents();
+
     firebaseAdminSetup();
   }
 
   public start() {
     //listen the server on a local port
-    this.app.listen(process.env.API_PORT, () => {
+    //this was app.listen before
+    this.server.listen(process.env.API_PORT, () => {
       console.log(`Server started on http://localhost:${process.env.API_PORT}`);
     });
   }
@@ -54,30 +71,28 @@ export class Server {
 
     //TEMPORARY, CHANGE LATER
     this.app.post("/v1/note/read", isAuthenticated, async (req, res) => {
-        const { noteId } = req.body;
-    
-        console.log(noteId)
-    
-        const note = await prisma.note.findFirst({
-          where: {
-            id: noteId,
-          },
-        });
+      const { noteId } = req.body;
 
-        console.log(res.locals.user)
-        console.log(note)
-    
-        if (!note) {
-          res.json({ success: false, message: "That note doesn't exist!" });
-        }
-    
-        res.json({ success: true, note: note });
+      console.log(noteId);
+
+      const note = await prisma.note.findFirst({
+        where: {
+          id: noteId,
+        },
+      });
+
+      // console.log(res.locals.user);
+      // console.log(note);
+
+      if (!note) {
+        res.json({ success: false, message: "That note doesn't exist!" });
+      }
+
+      res.json({ success: true, note: note });
     });
 
     this.app.put("/v1/note/update", async (req, res) => {
       const { noteId, body } = req.body;
-
-      console.log(noteId)
 
       const note = await prisma.note.update({
         where: {
@@ -85,11 +100,11 @@ export class Server {
         },
 
         data: {
-          body: body
-        }
+          body: body,
+        },
       });
 
-      console.log(note)
+      console.log(note);
 
       if (!note) {
         res.json({ success: false, message: "That note doesn't exist!" });
@@ -110,5 +125,49 @@ export class Server {
     });
 
     this.app.use("/v1", apiV1);
+  }
+
+  private registerSocketEvents() {
+    this.io.on("connection", async (socket) => {
+      console.log("Client connected to socket.io");
+      const { authToken } = socket.handshake.auth;
+      const { noteId } = socket.handshake.query;
+
+      const user = await FirebaseAdmin.auth().verifyIdToken(authToken);
+      try {
+        if (user) {
+          const note = await prisma.note.findFirst({
+            where: {
+              id: noteId.toString(),
+              userUid: user.uid,
+            },
+          });
+          await socket.join(note.id);
+        } else {
+          console.log("User does not exist");
+          socket.disconnect();
+        }
+      } catch (err) {
+        console.log(err);
+
+        socket.disconnect();
+      }
+
+      socket.on("update", async (body: string) => {
+        socket.broadcast.to(noteId).emit("update", body);
+        const res = await prisma.note.update({
+          where: {
+            id: noteId.toString(),
+          },
+          data: {
+            body: body,
+          },
+        });
+
+        if (!res) {
+          socket.disconnect();
+        }
+      });
+    });
   }
 }
