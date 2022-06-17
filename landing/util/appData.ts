0924f7b4215ea0df29d4f03a38bc4b085dc2c484
Shubham Patil <shubhampatilsd@gmail.com>
2022-06-16 20:43:42 -0700

    logo

--- landing/util/appData.ts
+++ landing/util/appData.ts
@@ -2,9 +2,9 @@
 export const appData = {
   name: "Notium",
   get description() {
-    return `${this.name} is the all-in-one platform for trip management. Manage packing lists, schedule your day, and keep track of your bookings in one central place.`;
+    return `${this.name}, revolutionalized notetaking for CS.`;
   },
   url: "https://notium.vercel.app",
   keywords:
-    "trip,trip management,tripley,trip planner,how to easily plan trip,booking manager,group trip platform", //placeholder, change later
+    "notes,notetaking,computer science,java,notetaking app for coding,notion alternatives,cs notetaking", //placeholder, change later
 };
