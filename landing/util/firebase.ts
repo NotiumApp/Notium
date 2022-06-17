7de5495f3b4ea9677f9e66a128a4f9b06e2b039f
Shubham Patil <shubhampatilsd@gmail.com>
2022-06-16 20:32:51 -0700

    cahned firebase key possibly undefined

--- landing/util/firebase.ts
+++ landing/util/firebase.ts
@@ -5,7 +5,10 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: (process.env.FIREBASE_PRIVATE_KEY || "").replace(
+        /\\n/g,
+        "\n"
+      ),
     }),
   });
 }
