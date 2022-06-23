8afaa4c4638f75ee0558c8cb722432ec0e7ee377
Shubham Patil <shubhampatilsd@gmail.com>
2022-06-23 13:30:21 -0700

    netlify

--- web/pages/login.tsx
+++ web/pages/login.tsx
@@ -1,7 +1,6 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
-// import styles from "../styles/Login.module.css";\
 import { FaGithub } from "react-icons/fa";
 
 import {
